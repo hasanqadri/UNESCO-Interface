180bd05ac14e08dc724e7b12b307212a43561ba1
Hasan Qadri <hqadri6@gatech.edu>
2020-04-17 19:50:27 -0400

    World map fits data from db

--- js/controller.js
+++ js/controller.js
@@ -7,21 +7,71 @@ var db = null
  * Change setupDBConnection() firebaseConfig to the correct configuration posted on slack
  */
 function controller() {
-    db = setupDBConnection()
+    db = setupDBConnection();
     if (db == null) {
         createWorldMap()
     } else {
         //We use a promise to retrieve the data, otherwise creatWorldMap() is called before the callback is complete
         populationDBCall().then(result => {
-            console.log(result)
+            console.log(result);
             createWorldMap(result)
         });
     }
 }
 
+function setupDBConnection() {
+    useMockData = true
+    if (useMockData) {
+        return null
+    } else {
+        // Set the configuration for your app
+        // TODO: See slack general for the config to be pasted below, do not push to github with this not removed
+        const firebaseConfig = { //EDITED OUT FOR NOW};
+        firebase.initializeApp(firebaseConfig);
+        // Get a reference to the database service
+        //How to retrieve data with firestore: https://firebase.google.com/docs/database/admin/retrieve-data
+        //Example with retrieving population
+        return firebase.firestore();
+    }
+}
+
+/**
+ * Example db call
+ * Uses the global db connection to retrieve documents in the "DEM_DATA_NATIONAL" collection
+ * Then it maps the documents matching the parameters to an array
+ * This array is of objects like thus:
+ *  [{INDICATOR_ID:"200101", YEAR:"2015", COUNTRY_ID:"USA"}, {...}, ...]
+ */
+async function populationDBCall() {
+    //Get label
+
+    var populationData = db.collection("DEM_DATA_NATIONAL")
+        .where("INDICATOR_ID", "==", "200101")
+        .where("YEAR", "==", "2015");
+    //The above query gets population data for the year 2015
+    let popData = await populationData.get().then(function(querySnapshot) {
+        console.log(querySnapshot.size);
+        if (querySnapshot.size > 0) {
+            return querySnapshot.docs.map(function (documentSnapshot) {
+                    return documentSnapshot.data(); //Each individual object (country's stat)
+            })
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
+        });
+
+    return popData
+}
+
+
+//On change handlers functions
+
+
 //On change handler for the factors triggers this method
 $( "#factor" ).change(function() {
-    alert( "Handler for factor called with " + $("#factor").val() + " chosen");
     //update data and transition colors
     if (view === 'World') {
         updateWorldMap();
@@ -35,7 +85,6 @@ $( "#factor" ).change(function() {
 
 //on change handler for the view (world, region, country) triggers this method
 $( "#view" ).change(function() {
-    alert( "Handler for view called with " + $("#view").val() + " chosen");
     let view = $("#view").val();
     //Change view of the current data
     if (view === 'World') {
@@ -57,55 +106,8 @@ $( "#view" ).change(function() {
 
 //on change handler for the country or region specific views triggers this method
 $( "#inputRegion" ).change(function() {
-    alert(  $("#inputRegion").val());
     //Change view of the current data to country and display the currently selected country
     $("#country").show();
     $("#region").hide();
     $("#worldMap").hide();
 });
-
-function setupDBConnection() {
-    useMockData = true
-    if (useMockData) {
-        return null
-    } else {
-        // Set the configuration for your app
-        // TODO: See slack general for the config to be pasted below, do not push to github with this not removed
-        const firebaseConfig = { //EDITED OUT FOR NOW
-        };
-        firebase.initializeApp(firebaseConfig);
-        // Get a reference to the database service
-        //How to retrieve data with firestore: https://firebase.google.com/docs/database/admin/retrieve-data
-        //Example with retrieving population
-        return firebase.firestore();
-    }
-}
-
-/**
- * Example db call
- * Uses the global db connection to retrieve documents in the "DEM_DATA_NATIONAL" collection
- * Then it maps the documents matching the parameters to an array
- * This array is of objects like thus:
- *  [{INDICATOR_ID:"200101", YEAR:"2015", COUNTRY_ID:"USA"}, {...}, ...]
- */
-async function populationDBCall() {
-    //Get label
-    var populationData = db.collection("DEM_DATA_NATIONAL")
-        .where("INDICATOR_ID", "==", "200101")
-        .where("YEAR", "==", "2015");
-    //The above query gets population data for the year 2015
-    let popData = await populationData.get().then(function(querySnapshot) {
-        if (querySnapshot.size > 0) {
-            return querySnapshot.docs.map(function (documentSnapshot) {
-                    return documentSnapshot.data(); //Each individual object (country's stat)
-            })
-        } else {
-            // doc.data() will be undefined in this case
-            console.log("No such document!");
-        }
-        }).catch(function(error) {
-            console.log("Error getting document:", error);
-        });
-    return popData
-}
-
