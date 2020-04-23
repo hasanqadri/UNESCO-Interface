52a9a346e8b31fed27ddb79ae6d659e22377fcce
Shubhi Agarwal <shubhi@Shubhis-MacBook-Air.local>
2020-04-22 23:21:25 -0400

    Add hand pointer

--- js/world-map.js
+++ js/world-map.js
@@ -73,13 +73,15 @@ function createWorldMap(dataSet) {
             tip.show(d);
           d3.select(this)
             .style('opacity', 1)
-            .style('stroke-width', 3);
+            .style('stroke-width', 3)
+            .style('cursor', 'pointer');
         })
         .on('mouseout', function(d){
           tip.hide(d);
           d3.select(this)
             .style('opacity', 0.8)
-            .style('stroke-width',0.3);
+            .style('stroke-width',0.3)
+            .style('cursor', 'default');
         })
         .on('click', function(d) {
             if (d.data != "Not available") {
